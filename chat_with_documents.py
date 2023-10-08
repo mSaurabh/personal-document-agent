d4b2f0f5ec1aec0f929c416178ec6f6a15610ed7
Saurabh Mankar <mankar.saurabh@gmail.com>
2023-10-08 17:23:04 -0500

    [Updated 10/8/23] Added pysqlite3 to get rid of Chroma import error for sqllite

--- chat_with_documents.py
+++ chat_with_documents.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
-import chromadb
 import os
 __import__('pysqlite3')
 import sys
