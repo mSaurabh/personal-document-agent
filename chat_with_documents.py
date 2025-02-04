bc187785b4db6330cb007bebd089460078a35e44
Saurabh Mankar <saurabh.mankar@oncor.com>
2025-02-04 09:46:05 -0600

    Fixed Import Error for Text Splitter

--- chat_with_documents.py
+++ chat_with_documents.py
@@ -24,7 +24,7 @@ def load_document(file):
     return data
 
 def chunk_data(data, chunk_size=256, chunk_overlap=20):
-    from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+    from langchain_community.text_splitters import RecursiveCharacterTextSplitter
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
                                                    chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_documents(data)
